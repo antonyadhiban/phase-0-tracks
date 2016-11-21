e1bc90055382d5efac4e32131cecfc0ed7081b2b
Antony Adhiban <gtadhib@gmail.com>
2016-11-21 13:23:57 +0530

    Add 4.4 Release 1 content to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+# Release 1 : 
+
+puts "What is your name?"
+name=gets.chomp!
+puts "How old are you?What year were you born"
+age=gets.chomp!
+year=gets.chomp!
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+order=gets.chomp!
+puts "Would you like to enroll in the company’s health insurance?"
+enroll=gets.chomp!
