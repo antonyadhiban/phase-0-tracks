01c1426993cd6fdb57925f699ecc26a6291d0b67
Antony Adhiban <gtadhib@gmail.com>
2016-11-29 23:07:01 +0530

    6.2 - Release 0 - Add behaviours to Puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,28 @@ class Puppy
     toy
   end
 
+  def speak(n)
+  	n.times do puts "Woof!" end
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+  	dog_years=human_years*7
+  	puts "#{human_years} human years is #{dog_years} dog years"
+  end
+
+  def play_dead
+  	puts "Lays on the ground"
+  end
+
 end
 
+pup=Puppy.new
+pup.fetch("car")
+pup.speak(3)
+pup.roll_over
+pup.dog_years(10)
+pup.play_dead
