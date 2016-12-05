e08e8f9e870ecd7dbf659ec05bd5accc30d4f1d3
Antony Adhiban <gtadhib@gmail.com>
2016-12-06 02:40:09 +0530

    Add driver code to make game structure

--- ruby/game/game_ruby.rb
+++ ruby/game/game_ruby.rb
@@ -1,25 +1,30 @@
 class WordGame
   
-  attr_accessor :no_of_guesses
+  attr_reader :word,:no_of_guesses,:answer_array,:hidden_answer
   
   def get_word(word)
     @word=word
     @answer_array=@word.split('')
-    no_of_guesses=(@answer_array.length*1.3).round
+    @no_of_guesses=(@answer_array.length*1.3).round
     @hidden_answer=[]
     @answer_array.length.times do 
-    	@hidden_answer.push('_') end
-    	puts @hidden_answer
+      @hidden_answer.push('_') end
+      @hidden_answer.each do |x| print x+" " end
+      puts
     puts "Player2, You get #{no_of_guesses} guesses"
   end
 
   def play_game(player_choice)
     @player_choice=player_choice
-    for i in @answer_array do
-      if i==@player_choice 
-        @hidden_answer[@answer_array.index(@player_choice)]=@player_choice
+      for item in @answer_array do
+        if item == @player_choice 
+          @hidden_answer[@answer_array.index(@player_choice)]=@player_choice
+          @answer_array[@answer_array.index(@player_choice)]="-"
+        break if item==@player_choice
+        end
       end
-    end
+    @hidden_answer.each do |x| print x+" " end
+    puts
   end
 
 end
@@ -29,5 +34,20 @@ word=gets.chomp!
 wordgame=WordGame.new 
 wordgame.get_word(word)
 
+count=wordgame.no_of_guesses
 
-wordgame.play_game('w')
+until count<0 do
+  puts "Enter an alphabet"
+  letter=gets.chomp!
+  wordgame.play_game(letter)
+  count-=1
+  break if wordgame.word==wordgame.hidden_answer.join('')
+end
+
+if wordgame.word==wordgame.hidden_answer.join('') 
+  puts "You have won"
+else
+  puts "Loooooser"
+end
+  
+  
--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -5,4 +5,10 @@ describe WordGame do
 		wordgame=WordGame.new
 		expect(wordgame.get_word("abc")).to eq ["_","_","_"]
 	end
+
+	it "gets an alphabet from user and checks if alphabet is present in input word " do
+		wordgame=WordGame.new
+		expect(wordgame.play_game('a')).to eq []
+	end
 end
+
