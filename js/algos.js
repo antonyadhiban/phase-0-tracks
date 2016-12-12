67ee09d704e28f9c5ddf51758a87b8461e1d1515
Antony Adhiban <gtadhib@gmail.com>
2016-12-12 21:24:51 +0530

    Create method to find if any key value pair matches between two objects

--- js/algos.js
+++ js/algos.js
@@ -10,6 +10,25 @@ function longest_phrase(phrase){
 	console.log(longest_phrase);	
 }
 
-//check above method using driver code
+function key_value_match(object1,object2){
+  
+  var i=0;
+  var result=false;
+  
+  while(i<Object.keys(object2).length){
+    if(object1[Object.keys(object1)[i]]==object2[Object.keys(object2)[i]]){
+	    result=true;
+	  }  
+	else{
+	  	result=false;
+	  }
+	  i+=1;
+  }
+  return result;
+}
+
+//check above methods using driver code
 
 longest_phrase(["hello","how are you","I am fine"]);
+
+key_value_match( {name:"antony",age:20} , {name:"antny",age:20} );
