057d6cf0e45092de054146b8ae9c26f7a3e7c891
Antony Adhiban <gtadhib@gmail.com>
2016-12-12 22:21:42 +0530

    Create method to generate random words and pass the words to longest_phrase method

--- js/algos.js
+++ js/algos.js
@@ -7,7 +7,7 @@ function longest_phrase(phrase){
 			longest_phrase=phrase[i];
 		}
 	}
-	console.log(longest_phrase);	
+	console.log("The longest phrase is "+longest_phrase);	
 }
 
 function key_value_match(object1,object2){
@@ -24,11 +24,36 @@ function key_value_match(object1,object2){
 	  }
 	  i+=1;
   }
+
   return result;
 }
+
+function generate_random_words(number_of_words){
+	
+	var words_array=[];
+	var random_word="";
+	var random_word_array=[];
+	
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  for(var i=0;i<number_of_words;i+=1){
+    for(var j=0;j<Math.floor(Math.random() * 10) + 1;j+=1){
+      random_word_array.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
+      random_word=random_word_array.join("");
+    }
+    random_word_array=[];
+    words_array.push(random_word);
+  }
+  console.log("The array of random words: \n"+words_array);
+  return words_array;
+}
+
 
 //check above methods using driver code
 
 longest_phrase(["hello","how are you","I am fine"]);
 
 key_value_match( {name:"antony",age:20} , {name:"antny",age:20} );
+
+
+
+longest_phrase(generate_random_words(10));
