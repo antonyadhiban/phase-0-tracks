90d829fef0f421684d68bb49cac55063949e60b5
Antony Adhiban <gtadhib@gmail.com>
2016-12-19 22:33:15 +0530

    create nested datastructure and access the elements inside it

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,13 @@
+# Nested datastructure
+nested_datastructure = {
+    # value as a string
+    ars:"Arsene Wenger",
+    # value as an array
+    mun:["Alex Ferguson","David Moyes","Van Gal","Jose Mourinho"],
+    # value as a hash
+    liv:{now:"Jurgen Klopp",before:"long list of managers"}
+    }
+# Nested access
+p nested_datastructure[:ars]
+p nested_datastructure[:mun][3]
+p nested_datastructure[:liv][:before]
